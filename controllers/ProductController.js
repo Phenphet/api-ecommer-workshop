64a7c0fac47cd05f6c185ed3b69130fb6756fd8d
Phenphet <potpt1098@gmail.com>
2025-07-09 16:58:59 +0700

    productModel and Controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,13 +7,13 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-app.post('/create', async(req, res) => {
+app.post('/create', async (req, res) => {
     try {
         await prisma.product.create({
             data: req.body
         })
-        res.send({message: 'success'})
-    } catch(e) {
+        res.send({ message: 'success' })
+    } catch (e) {
         res.status(500).send({ error: e.message })
     }
 })
@@ -23,13 +23,47 @@ app.get('/list', async (req, res) => {
         const data = await prisma.product.findMany({
             orderBy: {
                 id: 'desc'
+            },
+            where: {
+                status: 'use'
             }
         })
 
-        res.send({results: data})
+        res.send({ results: data })
     } catch (e) {
         res.status(500).send({ error: e.message })
     }
 })
 
+app.delete('/remove/:id', async (req, res) => {
+    try {
+        await prisma.product.update({
+            data: {
+                status: 'delete'
+            },
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+
+        res.send({ message: 'success' })
+    } catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
+
+app.put('/update', async (req, res) => {
+    try {
+        await prisma.product.update({
+            data: req.body,
+            where: {
+                id: parseInt(req.body.id)
+            }
+        })
+
+        res.send({ message: 'success' })
+    } catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
 module.exports = app
